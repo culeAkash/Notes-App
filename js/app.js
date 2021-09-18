9f7cdcc67d8c1a11b7c1b744733bc8b4bbe5ea70
Akash <akashjais929@gmail.com>
2021-09-18 21:13:10 +0530

    Added delete and search notes feature and increased responsiveness

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 console.log('notes-app');
 
-showNotes();
+showNotes();//after reload show the cards automatically
 
 //If user adds a note, add it to the local storage
 let addBtn = document.getElementById('add-btn');
 
+//adding notes
 addBtn.addEventListener('click', function (e) {
-    let addText = document.getElementById('addTxt');
+    let addText = document.getElementById('addTxt');//fetch the text area from which i have to take the text
     let notes = localStorage.getItem("notes");
 
     if (notes == null) {
@@ -15,9 +16,10 @@ addBtn.addEventListener('click', function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
+    //notesObj is the array of the texts stored in the local storage
 
-    notesObj.push(addText.value);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
+    notesObj.push(addText.value);//taking the text
+    localStorage.setItem('notes', JSON.stringify(notesObj));// after updating the array putting the array back into the local storage
     addText.value = '';
     console.log(notesObj);
 
@@ -25,7 +27,7 @@ addBtn.addEventListener('click', function (e) {
 
 })
 
-
+//function to show elements from local storage
 function showNotes() {
     let notes = localStorage.getItem('notes');
 
@@ -36,25 +38,25 @@ function showNotes() {
         notesObj = JSON.parse(notes);
     }
 
-    let html = "";
+    let html = "";//
 
     notesObj.forEach(function (element, index) {
         html += `
-        <div class=" notesCard my-2 mx-2 card" style="width: 18rem">
+        <div class=" notesCard my-2 mx-2 card text-white bg-dark" style="width: 18rem">
           <div class="card-body">
             <h5 class="card-title">Note ${index + 1}</h5>
             <p class="card-text">
               ${element}
             </p>
-            <a href="#" class="btn btn-primary">Delete Note</a>
+            <a id=${index} onclick="deleteNode(this.id)" class="btn btn-outline-light">Delete Note</a>
           </div>
         </div>
         `
-    });
+    });//we will have no of cards = no of text in the local storage
 
     let notesElm = document.getElementById('notes');
     if (notesObj.length != 0) {
-        notesElm.innerHTML = html;
+        notesElm.innerHTML = html;//getting the element in which there will be all the cards and then changing its innerhtml to show all the cards
     }
     else{
         notesElm.innerHTML= `Nothing to show! Add a note `
@@ -62,3 +64,47 @@ function showNotes() {
 
 
 }
+
+
+//function to delete node
+function deleteNode(index){
+    console.log('I am deleting', index);
+
+    let notes = localStorage.getItem('notes');
+
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index,1);//to delete specific elements from the array
+    localStorage.setItem('notes',JSON.stringify(notesObj));
+    showNotes();
+
+}
+
+
+let search= document.getElementById('searchTxt');
+search.addEventListener("input",function(){
+    let inputVal= search.value;
+
+    let noteCards= document.getElementsByClassName('notesCard');//getting all the elements with notesCard class
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt= element.getElementsByTagName("p")[0].innerText;//taking the paragraph element of all the noteCards
+        // console.log(cardTxt);
+        if(cardTxt.includes(inputVal)){//if the searched value is present in the para
+            element.style.display="block";
+        }
+        else{
+            element.style.display="none";
+        }
+    })
+})
+
+
+/**
+ * 1. Add Title
+ * 2. 
+ */
