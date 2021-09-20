bd88610e15e048c4d7187172ae3c3d89cbf2f231
Akash <akashjais929@gmail.com>
2021-09-20 11:33:56 +0530

    Now you can add Heading to your Note

--- js/app.js
+++ js/app.js
@@ -8,7 +8,10 @@ let addBtn = document.getElementById('add-btn');
 //adding notes
 addBtn.addEventListener('click', function (e) {
     let addText = document.getElementById('addTxt');//fetch the text area from which i have to take the text
+    let addHeading= document.getElementById('addTitle');
+    console.log(addHeading);
     let notes = localStorage.getItem("notes");
+    let headings= localStorage.getItem("headings");
 
     if (notes == null) {
         notesObj = [];
@@ -16,11 +19,21 @@ addBtn.addEventListener('click', function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
+
+    if (headings == null) {
+        headingsObj = [];
+    }
+    else {
+        headingsObj = JSON.parse(headings);
+    }
     //notesObj is the array of the texts stored in the local storage
 
     notesObj.push(addText.value);//taking the text
+    headingsObj.push(addHeading.value);
+    localStorage.setItem('headings',JSON.stringify(headingsObj));
     localStorage.setItem('notes', JSON.stringify(notesObj));// after updating the array putting the array back into the local storage
     addText.value = '';
+    addHeading.value='';
     console.log(notesObj);
 
     showNotes();
@@ -30,12 +43,19 @@ addBtn.addEventListener('click', function (e) {
 //function to show elements from local storage
 function showNotes() {
     let notes = localStorage.getItem('notes');
-
+    let headings= localStorage.getItem('headings');
     if (notes == null) {
         notesObj = [];
     }
     else {
         notesObj = JSON.parse(notes);
+    }
+
+    if (headings == null) {
+        headingsObj = [];
+    }
+    else {
+        headingsObj = JSON.parse(headings);
     }
 
     let html = "";//
@@ -44,7 +64,7 @@ function showNotes() {
         html += `
         <div class=" notesCard my-2 mx-2 card text-white bg-dark" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title">Note ${index + 1}</h5>
+            <h5 class="card-title">${headingsObj[index]}</h5>
             <p class="card-text">
               ${element}
             </p>
@@ -71,7 +91,14 @@ function deleteNode(index){
     console.log('I am deleting', index);
 
     let notes = localStorage.getItem('notes');
+    let headings= localStorage.getItem('headings');
 
+    if (headings == null) {
+        headingsObj = [];
+    }
+    else {
+        headingsObj = JSON.parse(headings);
+    }
     if (notes == null) {
         notesObj = [];
     }
@@ -80,7 +107,9 @@ function deleteNode(index){
     }
 
     notesObj.splice(index,1);//to delete specific elements from the array
+    headingsObj.splice(index,1);
     localStorage.setItem('notes',JSON.stringify(notesObj));
+    localStorage.setItem('headings',JSON.stringify(headingsObj));
     showNotes();
 
 }
@@ -92,9 +121,10 @@ search.addEventListener("input",function(){
 
     let noteCards= document.getElementsByClassName('notesCard');//getting all the elements with notesCard class
     Array.from(noteCards).forEach(function(element){
-        let cardTxt= element.getElementsByTagName("p")[0].innerText;//taking the paragraph element of all the noteCards
+        let cardTxt= (element.getElementsByTagName("p")[0].innerText).toLowerCase();//taking the paragraph element of all the noteCards
+        let cardheading= (element.getElementsByTagName("h5")[0].innerText).toLowerCase();
         // console.log(cardTxt);
-        if(cardTxt.includes(inputVal)){//if the searched value is present in the para
+        if(cardTxt.includes(inputVal) || cardheading.includes(inputVal)){//if the searched value is present in the para
             element.style.display="block";
         }
         else{
